e73e7e89e7c4219b33cef8b6d10b07eb090a4181
CrimsonDart <79547580+CrimsonDart@users.noreply.github.com>
2021-02-26 00:13:00 -0600

    /duelstart command added
    not functional yet tho

--- src/tk/pgfriends/duel/Main.java
+++ src/tk/pgfriends/duel/Main.java
@@ -1,13 +1,15 @@
 package tk.pgfriends.duel;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
+import tk.pgfriends.commands.CommandDuel;
 import tk.pgfriends.duel.events.PlayerEvents;
 
 public class Main extends JavaPlugin {
 	
 	public void onEnable()
 	{
+		this.getCommand("duelStart").setExecutor(new CommandDuel());
+		
 		getServer().getPluginManager().registerEvents(new PlayerEvents(), this);
 	}
 }
